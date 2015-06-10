08c77250cb0351cc68d09bf752f3d92a8dbdb05e
Thomas Ritter <thomas.ritter@simplificator.com>
2015-06-10 20:08:41 +0200

    Split up specs

--- spec/workbook_spec.rb
+++ spec/workbook_spec.rb
@@ -127,40 +127,4 @@ describe Bayonet::Workbook do
 
   end
 
-  context 'write different types' do
-
-    it 'writes a string to a cell' do
-      workbook.on_sheet('The Sheep') do |sheet|
-        sheet.write_string('A1', 'Yay')
-      end
-
-      Tempfile.open('output.xlsx') do |file|
-        workbook.write(file.path)
-        workbook.close
-
-        roo_xlsx = Roo::Excelx.new(file.path, :packed => false, :file_warning => :ignore)
-
-        sheet = roo_xlsx.sheet('The Sheep')
-        expect(sheet.cell('A', 1)).to eq('Yay')
-      end
-    end
-
-    it 'writes a number to a cell' do
-      workbook.on_sheet('The Sheep') do |sheet|
-        sheet.write_number('A2', 42)
-      end
-
-      Tempfile.open('output.xlsx') do |file|
-        workbook.write(file.path)
-        workbook.close
-
-        roo_xlsx = Roo::Excelx.new(file.path, :packed => false, :file_warning => :ignore)
-
-        sheet = roo_xlsx.sheet('The Sheep')
-        expect(sheet.cell('A', 2)).to eq(42)
-      end
-    end
-
-  end
-
 end
